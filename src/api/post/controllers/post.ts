5425676620fcf077399c90ca3e5deb026f3ff85b
AryanRajput-coder <kr357403@gmail.com>
2026-01-07 16:44:04 +0530

    feat: add default population for `createdBy` and `coverContent` to post queries

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -4,4 +4,32 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::post.post');
+export default factories.createCoreController('api::post.post', ({ strapi }) => ({
+    async find(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            populate: {
+                ...(typeof ctx.query.populate === 'object' ? ctx.query.populate : {}),
+                createdBy: {
+                    fields: ['firstname', 'lastname', 'username']
+                },
+                coverContent: true,
+            },
+        };
+        return super.find(ctx);
+    },
+
+    async findOne(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            populate: {
+                ...(typeof ctx.query.populate === 'object' ? ctx.query.populate : {}),
+                createdBy: {
+                    fields: ['firstname', 'lastname', 'username']
+                },
+                coverContent: true,
+            },
+        };
+        return super.findOne(ctx);
+    }
+}));
