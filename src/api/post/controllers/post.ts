83adc41cb074a41cbe79bc4d8b0e87123943147b
AryanRajput-coder <kr357403@gmail.com>
2026-01-08 14:20:52 +0530

    http 400 error

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -8,7 +8,9 @@ export default factories.createCoreController('api::post.post', ({ strapi }) => 
     async find(ctx) {
         // Strip createdBy from incoming query safely
         if (ctx.query.populate) {
-            if (typeof ctx.query.populate === 'object' && !Array.isArray(ctx.query.populate)) {
+            if (ctx.query.populate === 'createdBy' || ctx.query.populate === '*') {
+                delete ctx.query.populate;
+            } else if (typeof ctx.query.populate === 'object' && !Array.isArray(ctx.query.populate)) {
                 // @ts-ignore
                 delete ctx.query.populate.createdBy;
             } else if (Array.isArray(ctx.query.populate)) {
@@ -21,7 +23,9 @@ export default factories.createCoreController('api::post.post', ({ strapi }) => 
 
     async findOne(ctx) {
         if (ctx.query.populate) {
-            if (typeof ctx.query.populate === 'object' && !Array.isArray(ctx.query.populate)) {
+            if (ctx.query.populate === 'createdBy' || ctx.query.populate === '*') {
+                delete ctx.query.populate;
+            } else if (typeof ctx.query.populate === 'object' && !Array.isArray(ctx.query.populate)) {
                 // @ts-ignore
                 delete ctx.query.populate.createdBy;
             } else if (Array.isArray(ctx.query.populate)) {
